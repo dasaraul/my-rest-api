be75ac25b2b3c0bd8c400084eeabe7321469e67a
Tamaes <125809997+dasaraul@users.noreply.github.com>
2023-03-07 18:26:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'TamaesKun'// yourname
+global.creator = 'Tamaes'// yourname
 global.MONGO_DB_URI = "mongodb+srv://tamaes:tamakun@cluster0.rmegczb.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@iniapitama#20" //isi apa aja bebas
 global.your_email = "tamskun96@gmail.com" //email
-global.email_password = "Tamakun290#" //application password email
+global.email_password = "jifmahgegvoscyro" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
